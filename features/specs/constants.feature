c4cfc2e18144213e2dbeaa789f8cc7e540e83eed
Julio Santos <julio958214@gmail.com>
2022-02-15 15:14:31 -0300

    .

---
+++ features/specs/constants.feature
@@ -0,0 +1,6 @@
+#language:pt
+
+Funcionalidade: Massa de teste
+
+    Cenario: Logins no site 
+        Dado que o usuário esta logado com "valid_pass"
--- features/support/env.rb
+++ features/support/env.rb
@@ -5,7 +5,7 @@ require "rufo"
 require "selenium-webdriver"
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome_headless
+  config.default_driver = :selenium_chrome
   config.default_max_wait_time = 10
   config.app_host = "https://www.saucedemo.com"
 end
